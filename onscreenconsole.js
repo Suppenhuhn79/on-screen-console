68f65314d92d32fd924f6f8cc8efeb94dc07b4bc
Suppenhuhn79 <ch.zager@web.de>
2021-02-10 22:29:05 +0100

    Update onscreenconsole.js

--- onscreenconsole.js
+++ onscreenconsole.js
@@ -6,28 +6,29 @@ Copyright 2021 Christoph Zager, licensed under the Apache License, Version 2.0
 See the full license text at http://www.apache.org/licenses/LICENSE-2.0
  */
 
-if (/\bconsole=\d\b/i.test(window.location.search) === true)
+if (/\bconsole=\d\b/i.test(window.location.search))
 {
 	let _osc =
 	{
-		"VERSION": "v1.0",
-		"ELEMENT_ID": "on-screen-console",
-		"logLevel": Number(/\bconsole=(\d)\b/i.exec(window.location.search)[1]),
-		"styles":
+		VERSION: "v1.0",
+		ELEMENT_ID: "on-screen-console",
+		logLevel: Number(/\bconsole=(\d)\b/i.exec(window.location.search)[1]),
+		styles:
 		{
-			"#": "background-color: #006; color: #bbb; font-family: monospace; position: fixed; top: 0px; left: 0px; width: 100%; margin: 0rem; padding: 0.5rem; z-index: " + Number.MAX_SAFE_INTEGER + ";",
-			"prompt": "background-color: inherit; color: inherit; font-family: inherit; width: 100%; border: 1px solid #666; outline: none;",
-			"output": "white-space: pre; padding: 0rem; margin: 0rem; max-height: 60vh; overflow: scroll;",
-			"warn": "color: #dd6;",
-			"error": "color: #f66;",
-			"internal": "color: #fff; font-style: italic;",
-			"number": "color: #2d2;",
-			"object": "color: #3ff;",
-			"function": "color: #acf;",
-			"ITEM": "display: inline-block; vertical-align: top; margin-right: 0.7em;",
-			"ITEM:hover": "background-color: #008;"
+			"#": "background-color:#006;color:#bbb;font-family:monospace;font-size:small;position:fixed;top:0px;left:0px;width:100%;margin:0rem;padding:0.5rem;z-index:" + Number.MAX_SAFE_INTEGER + ";",
+			"prompt": "background-color:inherit;color:inherit;font-family:inherit;width:100%;border:1px solid #666;outline:none;",
+			"output": "white-space:pre;padding:0rem;margin:0rem;max-height:60vh;overflow:scroll;",
+			"warn": "color:#dd6;",
+			"error": "color:#f66;",
+			"internal": "color:#fff;font-style:italic;",
+			"number": "color:#2d2;",
+			"string": "",
+			"object": "color:#3ff;",
+			"function": "color:#acf;",
+			"ITEM": "display:inline-block;vertical-align:top;margin-right:0.7em;",
+			"ITEM:hover": "background-color:#337;"
 		},
-		"prefixes":
+		prefixes:
 		{
 			"error": "!",
 			"warn": "!",
@@ -37,12 +38,11 @@ if (/\bconsole=\d\b/i.test(window.location.search) === true)
 	};
 	if (_osc.logLevel > 0)
 	{
-		_osc.test = (a) => window.alert(a);
-		_osc.browserConsoleDebug = console.debug;
-		_osc.browserConsoleLog = console.log;
-		_osc.browserConsoleWarn = console.warn;
-		_osc.browserConsoleError = console.error;
-		_osc._log = (kind, ...values) =>
+		_osc.browserDbg = console.debug;
+		_osc.browserLog = console.log;
+		_osc.browserWrn = console.warn;
+		_osc.browserErr = console.error;
+		_osc._log = (rel, ...vals) =>
 		{
 			function __styleElement(element, style)
 			{
@@ -57,81 +57,96 @@ if (/\bconsole=\d\b/i.test(window.location.search) === true)
 					};
 				};
 			};
-			function __formatSimpleType(value, kind)
+			function __formatSimpleType(val, rel)
 			{
 				let span = document.createElement("span");
-				span.innerHTML = value;
-				let valueType = typeof value;
-				if (value === null)
+				span.innerHTML = val;
+				if (val === null)
 				{
 					span.innerHTML = "null";
 					span.classList.add("internal");
 				}
 				else
 				{
-					switch (valueType)
+					switch (typeof val)
 					{
 					case "undefined":
 					case "boolean":
 						span.classList.add("internal");
 						break;
 					case "string":
-						if (value === "")
+						if (val === "")
 						{
-							value = "&lt;empty string&gt;";
+							val = "&lt;empty string&gt;";
 							span.classList.add("internal");
 						}
 						else
 						{
-							value = value.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
-							if (kind === "result")
+							val = val.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
+							if (rel === "result")
 							{
-								value = "\"" + value + "\"";
+								span.classList.add("string");
+								val = "\"" + val + "\"";
 							};
 						};
-						span.innerHTML = value;
+						span.innerHTML = val;
 						break;
 					case "number":
 						span.classList.add("number");
-						span.innerHTML = value;
+						span.innerHTML = val;
 						break;
 					case "function":
 						span.classList.add("function");
-						span.innerHTML = "function" + /\(.*?\)/.exec(value.toString())[0].replaceAll(/[\r\n]/g, "");
+						span.innerHTML = "function" + /\(.*?\)/.exec(val.toString())[0].replaceAll(/[\r\n]/g, "");
 						break;
 					default:
 						span.classList.add("object");
-						span.innerHTML = "[object " + value.constructor.name + "]";
+						span.innerHTML = val.constructor.name;
 					};
 				};
-				span.classList.add("ITEM");
 				return span;
 			};
-			function __formattedValue(value, kind)
+			function __classMap(obj)
+			{
+				let result = [];
+				switch (obj.constructor.name.toUpperCase())
+				{
+				case "WINDOW":
+					result.push(obj.location);
+					break;
+				};
+				if (obj instanceof HTMLElement)
+				{
+					result.push(/<.+?>/.exec(obj.outerHTML)[0].replace("<", "&lt;").replace(">", "&gt;"));
+				};
+				return result;
+			};
+			function __formattedValue(val, rel)
 			{
 				let span = document.createElement("span");
-				if (value === null)
+				if (val === null)
 				{
-					span = __formatSimpleType(value, kind);
+					span = __formatSimpleType(val, rel);
 				}
 				else
 				{
-					switch (typeof value)
+					switch (typeof val)
 					{
 					case "object":
 						let titleSpan = document.createElement("span");
-						titleSpan.innerHTML = (value.constructor === Array) ? "Array(" + value.length + ")\n" : value.toString() + "\n";
+						titleSpan.innerHTML = (val.constructor === Array) ? "Array(" + val.length + ")" : val.constructor.name;
 						span.appendChild(titleSpan);
-						if ((value instanceof Error) === true)
+						if (val instanceof Error)
 						{
-							titleSpan.innerHTML = value;
-							if (value.stack !== undefined)
+							span.classList.add("error");
+							titleSpan.innerHTML = val;
+							if (val.stack !== undefined)
 							{
 								let stackSpan = document.createElement("ul");
 								__styleElement(stackSpan, "list-style: none; margin: 0rem; padding: 0rem;");
 								let li = document.createElement("li");
 								stackSpan.appendChild(li);
-								li.innerHTML = value.stack;
+								li.innerHTML = val.stack;
 								span.appendChild(stackSpan);
 							};
 						}
@@ -140,22 +155,33 @@ if (/\bconsole=\d\b/i.test(window.location.search) === true)
 							titleSpan.classList.add("object");
 							let ul = document.createElement("ul");
 							__styleElement(ul, "list-style: none; margin: 0rem; padding-left: 1.4em;");
-							for (let m in value)
+							let classMap = __classMap(val);
+							if (classMap.length > 0)
 							{
+								for (let m of classMap)
+								{
+									let li = document.createElement("li");
+									li.innerHTML = m;
+									ul.appendChild(li);
+								};
+							}
+							else
+							{
+								for (let m in val)
 								{
 									let li = document.createElement("li");
 									let keySpan = document.createElement("span");
 									keySpan.innerHTML = m + ":&nbsp;";
 									li.appendChild(keySpan);
-									li.appendChild(__formatSimpleType(value[m], kind));
+									li.appendChild(__formatSimpleType(val[m], "result"));
 									ul.appendChild(li);
 								};
-								span.appendChild(ul);
 							};
+							span.appendChild(ul);
 						};
 						break;
 					default:
-						span = __formatSimpleType(value, kind);
+						span = __formatSimpleType(val, rel);
 					};
 				};
 				span.classList.add("ITEM");
@@ -167,25 +193,25 @@ if (/\bconsole=\d\b/i.test(window.location.search) === true)
 			__styleElement(prefix, "white-space: nowrap; display: inline-block; vertical-align: top;");
 			let content = document.createElement("span");
 			__styleElement(content, "display: inline-block; vertical-align: top;");
-			prefix.innerHTML = (_osc.prefixes[kind] !== undefined) ? _osc.prefixes[kind] : "&nbsp;";
-			prefix.classList.add(kind);
-			switch (kind)
+			prefix.innerHTML = (_osc.prefixes[rel] !== undefined) ? _osc.prefixes[rel] : "&nbsp;";
+			prefix.classList.add(rel);
+			switch (rel)
 			{
 			case "input":
-				content.innerHTML = values[0];
+				content.innerHTML = vals[0];
 				break;
 			case "result":
-				content = __formattedValue(values[0], kind);
-				if ((values[0] instanceof Error) === true)
+				content = __formattedValue(vals[0], rel);
+				if (vals[0]instanceof Error)
 				{
 					content.classList.add("error");
 				};
 				break;
 			default:
-				content.classList.add(kind);
-				for (let v = 0, vv = values[0].length; v < vv; v += 1)
+				content.classList.add(rel);
+				for (let v = 0, vv = vals[0].length; v < vv; v += 1)
 				{
-					content.appendChild(__formattedValue(values[0][v], kind));
+					content.appendChild(__formattedValue(vals[0][v], rel));
 				};
 			};
 			prefix.innerHTML += "&nbsp;";
@@ -194,31 +220,31 @@ if (/\bconsole=\d\b/i.test(window.location.search) === true)
 			_osc.output.appendChild(div);
 		};
 		/* hijack browser default console functionions */
-		console.error = (...values) =>
+		console.error = (...vals) =>
 		{
-			_osc.browserConsoleError.apply(this, values);
-			_osc._log("error", values);
+			_osc.browserErr.apply(this, vals);
+			_osc._log("error", vals);
 		};
 		if (_osc.logLevel <= 3)
 		{
-			console.warn = (...values) =>
+			console.warn = (...vals) =>
 			{
-				_osc.browserConsoleWarn.apply(this, values);
-				_osc._log("warn", values);
+				_osc.browserWrn.apply(this, vals);
+				_osc._log("warn", vals);
 			};
 			if (_osc.logLevel <= 2)
 			{
-				console.log = (...values) =>
+				console.log = (...vals) =>
 				{
-					_osc.browserConsoleLog.apply(this, values);
-					_osc._log("info", values);
+					_osc.browserLog.apply(this, vals);
+					_osc._log("info", vals);
 				};
 				if (_osc.logLevel = 1)
 				{
-					console.debug = (...values) =>
+					console.debug = (...vals) =>
 					{
-						_osc.browserConsoleDebug.apply(this, values);
-						_osc._log("debug", values);
+						_osc.browserDbg.apply(this, vals);
+						_osc._log("debug", vals);
 					};
 				};
 			};
@@ -240,11 +266,11 @@ if (/\bconsole=\d\b/i.test(window.location.search) === true)
 		{
 			switch (keypressEvent.keyCode)
 			{
-				case 33: /* pg up */
+			case 33: /* pg up */
 				_osc.output.scrollBy(0, 0 - (_osc.output.clientHeight - 10));
 				break;
-				case 34: /* pg dn*/
-				_osc.output.scrollBy(0, (_osc.output.clientHeight -10));
+			case 34: /* pg dn*/
+				_osc.output.scrollBy(0, (_osc.output.clientHeight - 10));
 				break;
 			case 38: /* up */
 				if (_osc.historyPosition > 0)
@@ -281,26 +307,37 @@ if (/\bconsole=\d\b/i.test(window.location.search) === true)
 			{
 				keypressEvent.stopPropagation();
 				let cmd = keypressEvent.target.value;
-				if (cmd.startsWith(":") === true)
+				let intCmd = /^:(\w+)(?:\s+(.+))?/.exec(cmd);
+				_osc.historyPosition = _osc.history.push(cmd);
+				if (intCmd !== null)
 				{
-					switch (cmd)
+					switch (intCmd[1])
 					{
-					case ":clear":
+					case "clr":
 						for (let e of _osc.output.querySelectorAll("div"))
 						{
 							_osc.output.removeChild(e);
 						};
 						break;
-					case ":exit":
+					case "x":
+						console.debug = _osc.browserDbg;
+						console.log = _osc.browserLog;
+						console.warn = _osc.browserWrn;
+						console.error = _osc.browserErr;
 						document.body.removeChild(_osc.body);
 						break;
+					case "mh":
+						_osc.output.style.maxHeight = intCmd[2];
+						break;
+					case "fs":
+						_osc.body.style.fontSize = intCmd[2];
+						break;
 					default:
-						_osc._log("error", ["Unrecognized command " + cmd]);
+						_osc._log("error", ["Unrecognized internal command " + intCmd[1]]);
 					};
 				}
 				else
 				{
-					_osc.historyPosition = _osc.history.push(cmd);
 					_osc._log("input", cmd);
 					try
 					{
@@ -328,7 +365,7 @@ if (/\bconsole=\d\b/i.test(window.location.search) === true)
 			document.head.insertBefore(style, document.head.firstChild);
 			for (let rules in _osc.styles)
 			{
-				let selector = (rules.startsWith("#") === true) ? "#" + _osc.ELEMENT_ID : "#" + _osc.ELEMENT_ID + " ." + rules;
+				let selector = (rules.startsWith("#")) ? "#" + _osc.ELEMENT_ID : "#" + _osc.ELEMENT_ID + " ." + rules;
 				style.sheet.insertRule(selector + " { " + _osc.styles[rules] + " }");
 			};
 		}
